d6eba57e1696379fa6ea0dcd6f988915a3317fad
sunilpatro1985 <sunilpatro1985@gmail.com>
2020-05-22 18:53:35 +0530

    added sample login data

---
+++ testData/loginData.ts
@@ -0,0 +1,28 @@
+{   
+    "users":
+    [      
+        {   "env":"CI",         
+            "username":"ciemail@gmail.com",         
+            "password":"123!!!"
+        },
+       {    
+            "env":"PROD",         
+            "username":"prodemail@gmail.com",         
+            "password":"456!!!"
+        },
+        {
+            "env":"Stage",         
+            "username":"stageEmail@onvio.com",         
+            "password":"789!!!"
+        },
+        {
+            "env":"QA",         
+            "username":"qaemail@onvio.com",         
+            "password":"111!!!"
+        }   
+    ],   
+    "user":
+    {      
+        "env":"PROD"
+    }
+}
