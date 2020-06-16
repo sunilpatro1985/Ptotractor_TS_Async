e008d253e4df1ad3e93d7c90b30dd6be96b6bc99
sunilpatro1985 <sunilpatro1985@gmail.com>
2020-06-16 19:46:11 +0530

    cucumber_dataTableMethods

--- Features/sample.feature
+++ Features/sample.feature
@@ -1,12 +1,27 @@
 Feature: welcome to profile database
 
-Scenario Outline: Multiple profile entry from examples
-Given Profile entry screen
-Then Enter Name <Name>
-And Enter Tool <Tool>
-And Enter Exp <Exp>
-Then validate profiles saved with out error
-Examples:
+# Scenario Outline: Multiple profile entry from examples
+# Given Profile entry screen
+# Then Enter Name <Name>
+# And Enter Tool <Tool>
+# And Enter Exp <Exp>
+# Then validate profiles saved with out error
+# Examples:
+#     | Name | Tool | Exp | 
+#     | PQR | Protractor | 5 |
+#     | GHI | Appium | 6 |
+
+Scenario: Validate Profiles with DataTable
+Given Profiles report screen
+When Fetch the profiles
+Then Validate profiles
     | Name | Tool | Exp | 
+    | ABC  | Selenium | 7 |
+    | DEF  | Jenkins | 3 |
     | PQR | Protractor | 5 |
     | GHI | Appium | 6 |
+    
+    # | Key | Value|
+    # | UserName | "qavbox"|
+    # | Password | "qavbox123" |
+   
--- stepDef/Sample.ts
+++ stepDef/Sample.ts
@@ -1,4 +1,5 @@
-import {Given, When, Then} from "cucumber"
+import {Given, When, Then, TableDefinition} from "cucumber"
+var _ = require('lodash');
 
 Given(/^Profile entry screen$/, () => {
     console.log("Profile entry screen");
@@ -19,3 +20,34 @@ Given(/^Profile entry screen$/, () => {
   Then(/^validate profiles saved with out error$/, () => {
     console.log("validate profiles saved with out error");
   });
+
+  Given(/^Profiles report screen$/, () => {
+    console.log("Profiles report screen");
+  });
+
+  When(/^Fetch the profiles$/, () => {
+    console.log("Fetch the profiles");
+  });
+
+  Then(/^Validate profiles$/, (table: TableDefinition) => {
+    console.log("validate profiles - Display all profiles");
+
+    var rows = table.hashes(); //based on header fetch the value, header is the key
+    _.each(rows, function(row: any){
+      console.log(row.Name + " " + row.Tool + " " + row.Exp);
+    })
+
+    /* var rows = table.rows(); //retuns each row as object excluding 1st row
+    rows.forEach(function(value){
+      console.log(value[0] + " " + value[1] + " " + value[2]);
+    }) */
+
+    /* | Key | Value|
+    | UserName | "qavbox"|
+    | Password | "qavbox123" | 
+    var rows = table.rowsHash(); //table has to have 2 column data, excluding 1st row, column acts as key
+    console.log(rows.UserName);
+    console.log(rows.Password); */
+
+  });
+
